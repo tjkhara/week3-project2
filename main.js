a5a6df5756ed114ae28d29a3d43518b599ef1c00
bbouley <b.bouley@hotmail.co.uk>
2016-03-09 11:10:05 -0700

    split files into stretch goals

--- main.js
+++ main.js
@@ -32,18 +32,5 @@
 // 10. Declare a function called ohZero() that replaces all of the o's in a string with 0's.
       var string = "Soon you will master functions!";
 
-// 11. Write function that translates a sentence into pig latin.
-//     https://en.wikipedia.org/wiki/Pig_Latin
-//     i.e. "look at my cool function" --> "ooklay atyay ymay oolcay unctionfay"
-
-// 12. Write a function that prints out the entire "99 Bottles of Beer on the Wall" song lyrics.
-
-// 13. Create a 'Guessing Game'. The game starts by picking a random number.
-//    It then prompts the user to guess the number. If the user's number is lower/higher,
-//    it will prompt the user to enter another guess and tell the user if the guess was
-//    too high or too low. This continues until the correct guess is entered.
-//    When the correct guess is entered the user is given a success message with the correct number.
-
-
-// 14. http://games.usvsth3m.com/javascript-under-pressure/
+// 11. http://games.usvsth3m.com/javascript-under-pressure/
 //     Have fun with these! Get as far as you can and record your progress and time. We'll try this again in a few weeks!
---
+++ stretch.js
@@ -0,0 +1,14 @@
+// Stretch Goals
+
+// 1. Write function that translates a sentence into pig latin.
+//     https://en.wikipedia.org/wiki/Pig_Latin
+//     i.e. "look at my cool function" --> "ooklay atyay ymay oolcay unctionfay"
+
+// 2. Write a function that prints out the entire "99 Bottles of Beer on the Wall" song lyrics.
+
+// 3. Create a 'Guessing Game'. The game starts by picking a random number.
+//    It then prompts the user to guess the number. If the user's number is lower/higher,
+//    it will prompt the user to enter another guess and tell the user if the guess was
+//    too high or too low. This continues until the correct guess is entered.
+//    When the correct guess is entered the user is given a success message with the correct number.
+
