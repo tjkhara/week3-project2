937ad165aae9982a9d1dac5953ca21316039f6f7
Jenny Allar <jennyallar@gmail.com>
2016-04-13 10:58:26 -0600

    updating exercises - removing pig latin

--- main.js
+++ main.js
@@ -11,13 +11,13 @@
 // 4. Declare a function called multiply() that takes an array of numbers and multiplies them together.
 //    i.e. multiply([1, 2, 3, 4]) should return 24.
 
-// 5. Write a function called filterSixPlus() that takes the following array and returns words
-//    that are six characters or longer.
-      var words = ["window", "table", "barstool", "glass", "charger", "outlet"];
+// 5. Write a function called filterSixPlus() that takes the following array and removes words
+//    that are less than six characters.
 
 // 6. Use a function to ask a user for a temperature in Celsius and converts it to Fahrenheit.
+//    HINT: You may need to use parseFloat to convert the user's string input to a float.
 
-// 7. Adding to the code in #2, prompt the user a second time to determine whether they'd like to convert from Fahrenheit to Celsius
+// 7. Adding to the code in #6, prompt the user a second time to determine whether they'd like to convert from Fahrenheit to Celsius
 //    or from Celsius to Fahrenheit. To keep it simple, instruct them to use "F to C" or "C to F".
 //    Alert them with an error if they do not follow the rules.
 
@@ -25,25 +25,18 @@
 //    a number that indicates how many uppercase “B” characters are in the string.
 //    HINT: Google charAt()
 
-
 // 9. Write a function called countChars() that behaves like countBs(), except it takes a
 //    second argument that indicates what character is to be counted.
 
-// 10. Declare a function called ohZero() that replaces all of the o's in a string with 0's.
-      var string = "Soon you will master functions!";
+// 10. Declare a function called ohZero that replaces all of the o's in a string with 0's.
 
-// 11. Write function that translates a sentence into pig latin.
-//     https://en.wikipedia.org/wiki/Pig_Latin
-//     i.e. "look at my cool function" --> "ooklay atyay ymay oolcay unctionfay"
+// 11. Write a function that prints out the entire "99 Bottles of Beer on the Wall" song lyrics.
 
-// 12. Write a function that prints out the entire "99 Bottles of Beer on the Wall" song lyrics.
-
-// 13. Create a 'Guessing Game'. The game starts by picking a random number.
+// 12. Create a 'Guessing Game'. The game starts by picking a random number.
 //    It then prompts the user to guess the number. If the user's number is lower/higher,
 //    it will prompt the user to enter another guess and tell the user if the guess was
 //    too high or too low. This continues until the correct guess is entered.
 //    When the correct guess is entered the user is given a success message with the correct number.
 
-
-// 14. http://games.usvsth3m.com/javascript-under-pressure/
+// 13. http://games.usvsth3m.com/javascript-under-pressure/
 //     Have fun with these! Get as far as you can and record your progress and time. We'll try this again in a few weeks!
