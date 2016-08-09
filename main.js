b542f30615264bbee83c2104af0635d646af3e32
tjkhara <tkhara@gmail.com>
2016-08-09 16:47:00 -0700

    First 9 questions attempted
    first 9 attempted - time to save!
    
    Did three of the five questions for the times javascript challenge.

--- main.js
+++ main.js
@@ -81,24 +81,176 @@ console.log(sum(arrayOne));
 // 4. Declare a function called multiply() that takes an array of numbers and multiplies them together.
 //    i.e. multiply([1, 2, 3, 4]) should return 24.
 
+console.log("\nQuestion 4 \n");
+
+// 4. Declare a function called multiply() that takes an array of numbers and multiplies them together.
+//    i.e. multiply([1, 2, 3, 4]) should return 24.
+
+var muliplicationTotal = 1;
+
+function multiplyArray (array) {
+	for(i=0; i<array.length; i++) {
+		muliplicationTotal = array[i] * muliplicationTotal;
+	}
+	
+	console.log(muliplicationTotal);
+}
+
+multiplyArray([1,2,3,4]);
+
 // 5. Write a function called filterSixPlus() that takes the following array and removes words
 //    that are less than six characters.
 var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", "outlet"];
 
+console.log("\nQuestion 5 \n");
+
+// 5. Write a function called filterSixPlus() that takes the following array and removes words
+//    that are less than six characters.
+var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", "outlet"];
+
+function filterSixPlus(array) {
+	for(var i=array.length-1; i>=0; i--) {
+		if(array[i].length < 6) {
+			array.splice(i,1);
+		}
+	}
+	console.log(array);
+}
+
+filterSixPlus(words);
+
+
+
 // 6. Use a function to ask a user for a temperature in Celsius and converts it to Fahrenheit.
 //    HINT: You may need to use parseFloat to convert the user's string input to a float.
+
+console.log("\nQuestion 6 \n");
+
+// 6. Use a function to ask a user for a temperature in Celsius and converts it to Fahrenheit.
+//    HINT: You may need to use parseFloat to convert the user's string input to a float.
+
+function CelsiusToFahrenheit () {
+	var celsius = prompt("Enter the temperature in celcius");
+	var fahrenheit = (celsius * 9/5) + 32;
+	console.log(fahrenheit);
+}
+
+CelsiusToFahrenheit();
 
 // 7. Adding to the code in #6, prompt the user a second time to determine whether they'd like to convert from Fahrenheit to Celsius
 //    or from Celsius to Fahrenheit. To keep it simple, instruct them to use "F to C" or "C to F".
 //    Alert them with an error if they do not follow the rules.
 
+console.log("\nQuestion 7 \n");
+
+// 7. Adding to the code in #6, prompt the user a second time to determine whether they'd like to convert from Fahrenheit to Celsius
+//    or from Celsius to Fahrenheit. To keep it simple, instruct them to use "F to C" or "C to F".
+//    Alert them with an error if they do not follow the rules.
+
+function convertTemp () {
+	var tempEntry = prompt("Enter the temperature");
+	var whatDoYouWant = prompt("Would you like to convert F to C or C to F?")
+	if (whatDoYouWant === "C to F") {
+	var fahrenheit = (tempEntry * 9/5) + 32;
+	console.log(fahrenheit);
+	} else if (whatDoYouWant === "F to C") {
+		var celsius = (tempEntry - 32) * 5/9;
+		console.log(celsius);
+	} else {
+		alert("Please enter only \"F to C\" or \"C to F\"");
+		convertTemp();
+	}
+}
+
+convertTemp();
+
 // 8. Write a function countBs() that takes a string as its only argument and returns
 //    a number that indicates how many uppercase “B” characters are in the string.
 //    HINT: Google charAt()
 
+console.log("\nQuestion 8 \n");
+
+// 8. Write a function countBs() that takes a string as its only argument and returns
+//    a number that indicates how many uppercase “B” characters are in the string.
+//    HINT: Google charAt()
+
+function countBs(string) {
+	var counter = 0;
+	for(i=0;i<string.length;i++) {
+		if(string.charAt(i) === "B") {
+			counter++;
+		}
+	}
+	return counter;
+}
+
+countBs("BBBduhedoibeioB");
+
 // 9. http://games.usvsth3m.com/javascript-under-pressure/
 //     Have fun with these! Get as far as you can and record your progress and time. We'll try this again in a few weeks!
 
+console.log("\nQuestion 9 \n");
+// 9. http://games.usvsth3m.com/javascript-under-pressure/
+//     Have fun with these! Get as far as you can and record your progress and time. We'll try this again in a few weeks!
+
+function getFileExtension(i) {
+    
+    // i will be a string, but it may not have a file extension.
+    // return the file extension (with no period) if it has one, otherwise false
+    
+    if (isThereAPeriod(i)) {
+    	return grabCharactersAfterPeriod(i);
+    } else {
+    	return false;
+    }
+
+}
+
+getFileExtension("hello.png");
+
+
+function findPeriodPosition (string) {
+	
+	var position;
+    for(var k=0; k<string.length; k++) {
+        if(string.charAt(k) === ".") {
+        	return k;
+        }
+    
+	
+	}
+}
+
+function isThereAPeriod (string) {
+	
+	var trueFalse = false;
+	
+    for(var k=0; k<string.length; k++) {
+        if(string.charAt(k) === ".") {
+       	trueFalse = true;
+       	break;
+        } 
+	
+	}
+	
+	return trueFalse;
+}
+
+function grabCharactersAfterPeriod (string) {
+	if (isThereAPeriod(string)) {
+		var returnString = "";
+		var periodPosition = findPeriodPosition(string);
+		var stringLength = string.length;
+	
+		for (i=periodPosition+1; i<stringLength; i++) {
+			returnString = returnString.concat(string.charAt(i));
+		}
+		return returnString;
+		
+	} else {
+		return "There is no period in this string.";
+	}
+}
 
 // ADVANCED TRACK
 // 10. Write a function called countChars() that behaves like countBs(), except it takes a
